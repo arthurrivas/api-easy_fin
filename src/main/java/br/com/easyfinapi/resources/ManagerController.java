e0190cd2ae1e37a1f61b65228bad8a04a0459d6d
arthur rivas <arthurrivas1@gmail.com>
2022-09-18 23:24:38 -0300

    alterado crud manager

--- src/main/java/br/com/easyfinapi/resources/ManagerController.java
+++ src/main/java/br/com/easyfinapi/resources/ManagerController.java
@@ -7,79 +7,84 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.easyfinapi.domains.Manager;
 import br.com.easyfinapi.domains.DTO.ManagerDTO;
+import br.com.easyfinapi.security.UsuarioSS;
 import br.com.easyfinapi.services.ManagerService;
+import br.com.easyfinapi.services.UserServices;
 
 @RestController
 @RequestMapping(value = "/manager")
 public class ManagerController {
-	
+
 	@Autowired
 	ManagerService managerService;
-	
-	
+
+	@Autowired
+	UserServices userService;
+
 	@PostMapping
-	public ResponseEntity<?> createManager(@RequestBody ManagerDTO managerDTO){
-		
+	public ResponseEntity<?> createManager(@RequestBody ManagerDTO managerDTO) {
+
 		Manager manager = managerService.fromDTO(managerDTO);
-		
+
 		managerService.save(manager);
-		
-		
+
 		return new ResponseEntity<>(HttpStatus.CREATED).ok(manager);
-		
+
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<?> findManagerByid(@PathVariable(name = "id") Integer id){
-		
+	public ResponseEntity<?> findManagerByid(@PathVariable(name = "id") Integer id) {
+
 		try {
 			Manager manager = managerService.findById(id);
 
 			return new ResponseEntity(HttpStatus.OK).ok(manager);
-		
+
 		} catch (Exception e) {
 			return ResponseEntity.noContent().build();
 		}
 	}
-	
-	@DeleteMapping(value = "/{id}")
-	public ResponseEntity<?> deleteById(@PathVariable(name = "id") Integer id ) {
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<?> updateManager(@PathVariable(name = "id") Integer id, @RequestBody ManagerDTO managerDTO) {
 		try {
+			UsuarioSS ss = userService.isAuthenticated();
+			if (ss.getId() == id) {
+
+				Manager manager = managerService.fromDTO(managerDTO);
+				manager.setId(id);
+
+				managerService.save(manager);
+
+				return new ResponseEntity(HttpStatus.OK).ok(manager);
+			}
 			
-			managerService.deleteById(id);
-			
-			return ResponseEntity.noContent().build();
-			
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+
 		} catch (Exception e) {
-			
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> deleteById(@PathVariable(name = "id") Integer id) {
+		try {
+
+			managerService.deleteById(id);
+
+			return ResponseEntity.noContent().build();
+
+		} catch (Exception e) {
+
 			return null;
 		}
 	}
-	
-	
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
