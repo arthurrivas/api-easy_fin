fce6c984943af94d2a30b68d387dd3c6199179b6
arthur rivas <arthurrivas1@gmail.com>
2023-02-04 17:18:07 -0300

    add error handler

--- src/main/java/br/com/easyfinapi/services/UserServices.java
+++ src/main/java/br/com/easyfinapi/services/UserServices.java
@@ -1,5 +1,7 @@
 package br.com.easyfinapi.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
@@ -7,7 +9,7 @@ import org.springframework.stereotype.Service;
 import br.com.easyfinapi.domains.User;
 import br.com.easyfinapi.repositorys.UserRepository;
 import br.com.easyfinapi.security.UsuarioSS;
-import br.com.easyfinapi.services.exceptions.ObjNotFoundException;
+import br.com.easyfinapi.services.exceptions.UserNotFoundException;
 
 @Service
 public class UserServices {	
@@ -19,14 +21,22 @@ public class UserServices {
 		
 		UsuarioSS ss = (UsuarioSS) SecurityContextHolder.getContext().getAuthentication().getPrincipal();			
 		
-		if (ss == null || ss.getId() == null) throw new ObjNotFoundException("usuario não encontrado");
+		if (ss == null || ss.getId() == null) throw new UserNotFoundException("usuario não encontrado");
 		
 		return ss;
 	}
 
 	public User findById(Integer id) {
-		return userRepository.findById(id).get();
+
+		Optional<User> opt = userRepository.findById(id); 
+		
+		if (!opt.isPresent()) throw new UserNotFoundException("Usuario nao encontrado");
+
+		return opt.get();
 	}
 	
+	public void save(User user){
+		userRepository.save(user);
+	}
 
 }
