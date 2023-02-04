ccfefd90840e359583c89a9de124ed9f26bc8ec3
arthur rivas <arthurrivas1@gmail.com>
2023-02-04 17:30:42 -0300

    corrigido bug error handler

--- src/main/java/br/com/easyfinapi/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/easyfinapi/resources/exceptions/ResourceExceptionHandler.java
@@ -6,24 +6,27 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.easyfinapi.services.exceptions.ObjectNotFoundException;
-import br.com.easyfinapi.services.exceptions.ObjNotFoundException;
+import br.com.easyfinapi.services.exceptions.UserNotFoundException;
 
 @ControllerAdvice
-public class ResourceExceptionHandler {
+public class ResourceExceptionHandler  extends ResponseEntityExceptionHandler{
 
-	@ExceptionHandler(ObjectNotFoundException.class)
+	@ExceptionHandler({ObjectNotFoundException.class})
 	public ResponseEntity<StandartError> ObjectNotFound(ObjectNotFoundException e, HttpServletRequest req){
 		
 		StandartError err = new StandartError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 	
-	@ExceptionHandler(ObjNotFoundException.class)
-	public ResponseEntity<StandartError> UserNotFound(ObjectNotFoundException e, HttpServletRequest req){
+	@ExceptionHandler( {UserNotFoundException.class})
+	public ResponseEntity<StandartError> UserNotFound(UserNotFoundException e, HttpServletRequest req){
 		
-		StandartError err = new StandartError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+		StandartError err = new StandartError(HttpStatus.NO_CONTENT.value(), e.getMessage(), System.currentTimeMillis());
+
+		return new ResponseEntity<StandartError>(err, HttpStatus.NO_CONTENT);
 	}
 }
