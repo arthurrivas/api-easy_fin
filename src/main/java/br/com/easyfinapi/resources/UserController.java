42948a76c405221c8b2d08954733870fe3749375
arthur rivas <arthurrivas1@gmail.com>
2023-02-04 17:31:50 -0300

    find user by id

--- src/main/java/br/com/easyfinapi/resources/UserController.java
+++ src/main/java/br/com/easyfinapi/resources/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +26,18 @@ public class UserController {
 		
 		User user = userService.findById(ss.getId());
 		
-		return new ResponseEntity<>(HttpStatus.OK).ok(user);
+		return ResponseEntity.ok(user);
 
 	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<?> getUserById(@PathVariable(name = "id") Integer id) {
+
+		User user = userService.findById(id);
+
+		return new ResponseEntity<>(user, HttpStatus.OK);
+
+	}
+
 
 }
